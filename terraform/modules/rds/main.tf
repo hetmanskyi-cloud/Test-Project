aef7d6e055399a4eac6e7eb53a89cc34371a4adf
Hetmansky <hetmansky@gmail.com>
2024-10-19 22:07:12 +0000

    Enable encryption for Performance Insights and update Terraform configuration

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -18,13 +18,14 @@ resource "aws_db_instance" "mysql" {
   password                            = var.db_password
   db_subnet_group_name                = aws_db_subnet_group.default.name
   skip_final_snapshot                 = true
-  publicly_accessible                 = false # Avoid public access
-  backup_retention_period             = 30    # Set backup retention
-  multi_az                            = true  # Enable multi-AZ for high availability
-  storage_encrypted                   = true  # Enable storage encryption
-  deletion_protection                 = true  # Enable deletion protection
-  iam_database_authentication_enabled = true  # Enable IAM authentication
-  performance_insights_enabled        = true  # Enable Performance Insights
+  publicly_accessible                 = false                                                                         # Avoid public access
+  backup_retention_period             = 30                                                                            # Set backup retention
+  multi_az                            = true                                                                          # Enable multi-AZ for high availability
+  storage_encrypted                   = true                                                                          # Enable storage encryption
+  deletion_protection                 = true                                                                          # Enable deletion protection
+  iam_database_authentication_enabled = true                                                                          # Enable IAM authentication
+  performance_insights_enabled        = true                                                                          # Enable Performance Insights
+  performance_insights_kms_key_id     = "arn:aws:kms:eu-west-1:739275435828:key/fb56457e-49fd-446d-b266-6ea578b8d42d" # KMS Key ID for Performance Insights encryption
 
   vpc_security_group_ids = [var.security_group_id]
 
